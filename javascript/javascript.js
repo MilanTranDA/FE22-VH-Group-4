dffd0e04fbad9aa304c16da4c5c4a6c5134a0b87
Leon Tillberg <leon.tillberg.mlinaric@gritacademy.se>
2023-01-15 16:23:56 +0100

    Interactive map functioner

---
+++ javascript/javascript.js
@@ -0,0 +1,9 @@
+let myMap = L.map("map").setView([37.61, -122.011], 10);
+
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    // Attribution is obligatory as per copyright!
+	maxZoom: 20
+}).addTo(myMap);
+
