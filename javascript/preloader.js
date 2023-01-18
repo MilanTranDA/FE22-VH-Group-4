a12a615bd485ed061fcc289accfe79d5da8d4599
KoLario <felix.wong@gritacademy.se>
2023-01-18 23:24:37 +0100

    Added images, added theme switcher js,changed styling in about
    + theme switcher not fully implemented in html yet
    -flex on user card not fully responsive yet

--- javascript/preloader.js
+++ javascript/preloader.js
@@ -14,3 +14,32 @@ function toggleNav() {
         navbar.style.display = "block";
     }
 }
+
+//Toggle light and dark theme
+const toggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+// toggle theme on button click
+toggle.addEventListener('click', () => {
+  if (body.classList.contains('light-theme')) {
+    body.classList.remove('light-theme');
+    body.classList.add('dark-theme');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    body.classList.remove('dark-theme');
+    body.classList.add('light-theme');
+    localStorage.setItem('theme', 'light');
+  }
+});
+
+// check local storage on page load
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  body.classList.add(currentTheme + '-theme');
+} else if(matchMedia('(prefers-color-scheme: dark)').matches) {
+  body.classList.add('dark-theme');
+  localStorage.setItem('theme', 'dark');
+} else {
+  body.classList.add('light-theme');
+  localStorage.setItem('theme', 'light');
+}
