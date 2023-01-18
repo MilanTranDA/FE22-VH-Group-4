07d51fc60a768d0a0c2c7a56493f3393e97c87ca
austin <austin-hamilton17@hotmail.com>
2023-01-18 17:41:54 +0100

    navbar

--- javascript/preloader.js
+++ javascript/preloader.js
@@ -4,3 +4,13 @@ window.addEventListener("load", function () {
         document.querySelector("#main-content").style.display = "block";
     }, 1000);
 });
+
+
+function toggleNav() {
+    var navbar = document.getElementById("navbarNav");
+    if (navbar.style.display === "block") {
+        navbar.style.display = "none";
+    } else {
+        navbar.style.display = "block";
+    }
+}
