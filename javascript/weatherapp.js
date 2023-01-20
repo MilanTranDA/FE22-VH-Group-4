f45c3507c2141f014f875a1f2b156d59ca0ea33a
Leon Tillberg <112480830+LeonTillberg@users.noreply.github.com>
2023-01-20 09:51:37 +0100

    Fixade så att framsidan visar vädret i 5 dagar fram

--- javascript/weatherapp.js
+++ javascript/weatherapp.js
@@ -23,7 +23,7 @@ function searchForWeather(event) {
 }
 
 function fetchfunction(SearchedForcity) {
-const fetchUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${SearchedForcity}&units=metric&cnt=5&appid=c79d5ea95c63dffbb72a56fdbf56e59d`;
+    const fetchUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${SearchedForcity}&units=metric&cnt=5&appid=c79d5ea95c63dffbb72a56fdbf56e59d`;
 
     fetch(fetchUrl)
         .then(response => {
@@ -69,51 +69,56 @@ function showWeatherData(apiData) {
     smalleContainerShowingWeather.classList.add("smallContainerForWeather")
 
     //Showing the days for weather app. 
-    const weekday = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    
+    const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const date = new Date();
     let day = weekday[date.getDay()];
     const dayForWeather = document.createElement('h3');
     smalleContainerShowingWeather.appendChild(dayForWeather);
     dayForWeather.innerText = day;
     dayForWeather.classList.add("CurrentDayforWeather")
-
-    const weatherDescription = document.createElement('h5');
-    smalleContainerShowingWeather.appendChild(weatherDescription);
-    weatherDescription.innerText = apiData.list[0].weather[0].main;
-    weatherDescription.classList.add("weatherDescription")
-
-    const temperatureCelcius = document.createElement('h3');
-    smalleContainerShowingWeather.appendChild(temperatureCelcius);
-    temperatureCelcius.innerText = apiData.list[0].main.temp + ' °C';
-    temperatureCelcius.classList.add("temperatureCelcius")
-
-    const temperatureMax = document.createElement('h5');
-    smalleContainerShowingWeather.appendChild(temperatureMax);
-    temperatureMax.innerText = '↑  ' +  apiData.list[0].main.temp_max+ ' °C';
-    temperatureMax.classList.add("temperatureMax")
-
-    const temperatureMin = document.createElement('h5');
-    smalleContainerShowingWeather.appendChild(temperatureMin);
-    temperatureMin.innerText = '↓  ' +  apiData.list[0].main.temp_min+ ' °C';
-    temperatureMin.classList.add("temperatureMin")
-
-    const tempertureContainer = document.createElement('div');
-    tempertureContainer.classList.add('temperatureCon');
-    smalleContainerShowingWeather.appendChild(tempertureContainer);
-    tempertureContainer.appendChild(temperatureMax)
-    tempertureContainer.appendChild(temperatureMin)
-
-    const WeatherIcon = apiData.list[0].weather[0].icon;
-    const imageUrl = `http://openweathermap.org/img/wn/${WeatherIcon}@2x.png`
-    const showWeatherIconImage = document.createElement('img');
-    smalleContainerShowingWeather.appendChild(showWeatherIconImage);
-    showWeatherIconImage.src = imageUrl;
-    showWeatherIconImage.classList.add('weatherIcon')
-     }
+    
+    for (i = 0; i < 5; i++) {
+        const weatherDescription = document.createElement('h5');
+        smalleContainerShowingWeather.appendChild(weatherDescription);
+        weatherDescription.innerText = apiData.list[i].weather[0].main;
+        weatherDescription.classList.add("weatherDescription")
+    
+        const temperatureCelcius = document.createElement('h3');
+        smalleContainerShowingWeather.appendChild(temperatureCelcius);
+        temperatureCelcius.innerText = apiData.list[i].main.temp + ' °C';
+        temperatureCelcius.classList.add("temperatureCelcius")
+    
+        const temperatureMax = document.createElement('h5');
+        smalleContainerShowingWeather.appendChild(temperatureMax);
+        temperatureMax.innerText = '↑  ' + apiData.list[i].main.temp_max + ' °C';
+        temperatureMax.classList.add("temperatureMax")
+    
+        const temperatureMin = document.createElement('h5');
+        smalleContainerShowingWeather.appendChild(temperatureMin);
+        temperatureMin.innerText = '↓  ' + apiData.list[i].main.temp_min + ' °C';
+        temperatureMin.classList.add("temperatureMin")
+    
+        const tempertureContainer = document.createElement('div');
+        tempertureContainer.classList.add('temperatureCon');
+        smalleContainerShowingWeather.appendChild(tempertureContainer);
+        tempertureContainer.appendChild(temperatureMax)
+        tempertureContainer.appendChild(temperatureMin)
+    
+        const WeatherIcon = apiData.list[i].weather[0].icon;
+        const imageUrl = `http://openweathermap.org/img/wn/${WeatherIcon}@2x.png`
+        const showWeatherIconImage = document.createElement('img');
+        smalleContainerShowingWeather.appendChild(showWeatherIconImage);
+        showWeatherIconImage.src = imageUrl;
+        showWeatherIconImage.classList.add('weatherIcon')
+    
+        console.log(apiData.list[i])
+    }
+}
 
 
 
 function showErrorMessage(errorMessage) {
     error.innerText = errorMessage;
-    smalleContainerShowingWeather.innerHTML = '';
+    // smalleContainerShowingWeather.innerHTML = '';
 }
